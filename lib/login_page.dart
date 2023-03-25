a5babc00f1e44df136abf09a258e2dcb68bcfecb
Md. Shadiuzzaman Khan <68236926+Md-Shadiuzzaman-Khan@users.noreply.github.com>
2023-03-26 03:48:47 +0600

    Initial commit

---
+++ lib/login_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.fitbit,
+                size: 100,
+                color: Colors.deepPurple,
+              ),
+
+              SizedBox(height: 75,),
+
+              //welcome text
+
+              Text(
+                "Hello Again!",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Lobster',
+                ),
+              ),
+
+              SizedBox(height: 10,),
+
+              Text(
+                "Welcome back to your favourite App",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+
+              SizedBox(height: 50,),
+
+              //email textfield
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Email",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10,),
+
+              //password textfield
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Password",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 10,),
+
+              //signin button
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                    child: Center(
+                        child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                        ),
+                    ),
+                ),
+              ),
+
+              SizedBox(height: 25,),
+
+              //not register
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(" Register now",
+                    style: TextStyle(
+                      color: Colors.deepPurple[300],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
